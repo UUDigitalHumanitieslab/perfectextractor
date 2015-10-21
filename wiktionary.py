3fe3c8f86f0990a566a89f2015c06c9ed490b35a
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2015-10-21 16:40:30 +0200

    Moved retrieval of ergative verbs

--- wiktionary.py
+++ wiktionary.py
@@ -1,3 +1,4 @@
+import codecs
 import requests
 
 
@@ -14,6 +15,27 @@ def get_translations(word, language_from, language_to):
                 results.append(link['*'])
 
     return results
+
+
+def get_ergative_verbs():
+    title = 'Category:Ergatief_werkwoord_in_het_Nederlands'
+    with codecs.open('data/nl_aux_be.txt', 'wb', 'utf-8') as f:
+        continue_url = ''
+
+        params = dict({'action': 'query', 'list': 'categorymembers', 'cmtitle': title, 'format': 'json'})
+        while True:
+            params['cmcontinue'] = continue_url
+
+            r = requests.get('https://nl.wiktionary.org/w/api.php', params=params)
+            j = r.json()
+            for member in j['query']['categorymembers']:
+                f.write(member['title'] + '\n')
+
+            # Continue to next URL
+            if 'continue' not in j:
+                break
+            continue_url = j['continue']['cmcontinue']
+
 
 if __name__ == "__main__":
     print get_translations('aantonen', 'nl', 'en')
