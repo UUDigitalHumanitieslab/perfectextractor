17a8d4505765f43a09022504f3c193678e893414
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2020-06-03 12:50:34 +0200

    Updated the setup script

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+import setuptools
 
 
 def readme():
@@ -6,25 +6,28 @@ def readme():
         return f.read()
 
 
-setup(name='perfectextractor',
-      version='0.1',
-      description='Extracting present perfects (and related forms) from parallel corpora',
-      long_description=readme(),
-      classifiers=[
+setuptools.setup(
+    name='perfectextractor',
+    version='0.2',
+    author='Martijn van der Klis',
+    author_email='M.H.vanderKlis@uu.nl',
+    description='Extracting present perfects (and related forms) from parallel corpora',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
         'Topic :: Text Processing :: Linguistic',
-      ],
-      url='https://github.com/UUDigitalHumanitieslab/perfectextractor',
-      author='Martijn van der Klis',
-      author_email='M.H.vanderKlis@uu.nl',
-      license='MIT',
-      packages=['perfectextractor'],
-      install_requires=[
-          'click',
-          'lxml',
-          'requests',
-      ],
-      include_package_data=True,
-      zip_safe=False)
+    ],
+    url='https://github.com/UUDigitalHumanitieslab/perfectextractor',
+    license='MIT',
+    packages=['perfectextractor'],
+    python_requires='>=3.6',
+    install_requires=[
+      'click',
+      'lxml',
+      'requests',
+    ],
+)
